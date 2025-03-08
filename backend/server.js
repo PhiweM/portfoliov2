c7ac2a2371a3d8b7f9f120a238d3bcd034379669
Phiwe M <wycliffwhite7@gmail.com>
2025-03-07 19:50:21 -0500

    updated package.json

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,20 @@ const express = require('express');
 const cors = require('cors');
 const projectRoutes = require('./routes/projectRoutes');
 const connectDB = require('./config/db');
-
 const app = express();
+const path = require('path');
 
 // // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 // Routes
 app.use('/projects', projectRoutes);
