9a106e04d9bccf56818aadf61a120571ec86343f
Phiwe M <wycliffwhite7@gmail.com>
2025-04-22 14:26:00 -0400

    update for server routing for vercel

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,45 @@
+// import dotenv from 'dotenv';
+// import express from 'express';
+// import cors from 'cors';
+// import projectRoutes from './routes/projectRoutes.js';
+// import connectDB from './config/db.js';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+
+// // Get __dirname equivalent in ES modules
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Load environment variables
+// dotenv.config({path:'./backend/.env'});
+// // console.log("MONGO_URI:", process.env.MONGO_URI);
+
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use('/projects', projectRoutes);
+
+// app.use(express.static(path.join(__dirname, '../dist')));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../dist/index.html'));
+//   });
+  
+
+// // MongoDB Connection
+// connectDB();
+
+// // Server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
@@ -6,35 +48,32 @@ import connectDB from './config/db.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
-// Get __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Load environment variables
-dotenv.config({path:'./backend/.env'});
-// console.log("MONGO_URI:", process.env.MONGO_URI);
-
+dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/projects', projectRoutes);
-
-app.use(express.static(path.join(__dirname, '../dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
-  });
-  
-
-// MongoDB Connection
 connectDB();
 
-// Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.use('/projects', projectRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}
+
+// Important for Vercel
+export default app;
